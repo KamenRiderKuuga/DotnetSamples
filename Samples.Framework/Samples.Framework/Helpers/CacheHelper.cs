4bf75f96ef0e3d13293d3fb37e5f083b8d3397b0
GadHao <39508075+GadHao@users.noreply.github.com>
2021-02-19 23:59:01 +0800

    新增仓库Samples.Framework，用于记录一些.NET Framework内置对象的使用示例

---
+++ Samples.Framework/Samples.Framework/Helpers/CacheHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.Caching;
+
+namespace Samples.Framework.Helpers
+{
+    public class CacheHelper
+    {
+        private static ObjectCache _cache = MemoryCache.Default;
+
+        /// <summary>
+        /// 返回缓存管理器的实例
+        /// </summary>
+        public static ObjectCache Instance => _cache;
+
+        /// <summary>
+        /// 新增(若无)/替换(若已存在)一条现有的缓存记录
+        /// </summary>
+        /// <param name="key">缓存的键</param>
+        /// <param name="value">缓存的内容</param>
+        /// <param name="expireMinutes">缓存的过期时间(单位：分)</param>
+        public static void Set(string key, object value, int expireMinutes = 10)
+        {
+            if (value is null)
+            {
+                return;
+            }
+            string jsonValue = Newtonsoft.Json.JsonConvert.SerializeObject(value);
+            _cache.Set(key, jsonValue, DateTimeOffset.Now.AddMinutes(expireMinutes));
+        }
+
+        /// <summary>
+        /// 获取一条存在的缓存记录，若无，则返回指定类型的默认值
+        /// </summary>
+        /// <typeparam name="T">缓存内容的类型</typeparam>
+        /// <param name="key">缓存的键</param>
+        /// <returns></returns>
+        public static T Get<T>(string key)
+        {
+            var cacheItem = _cache.Get(key);
+
+            return cacheItem is null ? default(T) : Newtonsoft.Json.JsonConvert.DeserializeObject<T>(cacheItem.ToString());
+        }
+    }
+}
---
+++ Samples.Framework/Samples.FrameworkTests/Helpers/CacheHelperTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Samples.Framework.Helpers.Tests
+{
+    [TestClass()]
+    public class CacheHelperTests
+    {
+        /// <summary>
+        /// 一个学生类，测试用
+        /// </summary>
+        public class Student : IEquatable<Student>
+        {
+            public bool Equals(Student other)
+            {
+                return other?.Age == Age && other?.Name == Name;
+            }
+
+            /// <summary>
+            /// 年龄
+            /// </summary>
+            public int Age { get; set; }
+
+            /// <summary>
+            /// 姓名
+            /// </summary>
+            public string Name { get; set; }
+        }
+
+        [TestMethod()]
+        public void TestSetAddGetCaches()
+        {
+            CacheHelper.Set("Number", null);
+            CacheHelper.Set("Number", 2);
+
+            Student student = new Student() { Age = 18, Name = "新学生" };
+            CacheHelper.Set("Student", student);
+
+            string tempValue = "NewValue";
+            CacheHelper.Set("StringValue", tempValue);
+
+            Assert.AreEqual(3, CacheHelper.Instance.GetCount());
+            Assert.AreEqual(2, CacheHelper.Get<int>("Number"));
+            Assert.IsTrue(student.Equals(CacheHelper.Get<Student>("Student")));
+            Assert.AreEqual("NewValue", CacheHelper.Get<string>("StringValue"));
+
+            CacheHelper.Set("Student", student, 1);
+            // Sleep一分钟，然后查看结果
+            System.Threading.Thread.Sleep(60000);
+
+        }
+    }
+}
