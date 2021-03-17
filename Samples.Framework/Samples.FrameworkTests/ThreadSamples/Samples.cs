b201009a1835ec19dfe125d5d2ea85d1abbd167a
GadHao <39508075+GadHao@users.noreply.github.com>
2021-03-18 00:04:25 +0800

    Update Samples.cs

--- Samples.Framework/Samples.FrameworkTests/ThreadSamples/Samples.cs
+++ Samples.Framework/Samples.FrameworkTests/ThreadSamples/Samples.cs
@@ -10,22 +10,22 @@ namespace Samples.FrameworkTests.ThreadSamples
         [TestMethod("主线程和子线程同时运行")]
         public void MainThreadRunWithChild()
         {
-            Thread childThread = new Thread(writeThreadName);
+            Thread childThread = new Thread(WriteThreadName);
             childThread.Start();
 
-            writeThreadName();
+            WriteThreadName();
         }
 
         [TestMethod("只有一个前台线程运行")]
         public void OnlyOneForegroundThread()
         {
-            Thread childThread = new Thread(writeThreadName);
+            Thread childThread = new Thread(WriteThreadName);
             childThread.Start();
-            // 因为处于testhost，需要等待ForegroundThread执行完毕
+            // 因为处于当前线程处于Background线程，使用Join等待ForegroundThread执行完毕
             childThread.Join();
         }
 
-        public static void writeThreadName()
+        public static void WriteThreadName()
         {
             for (int i = 0; i < 10000; i++)
             {
