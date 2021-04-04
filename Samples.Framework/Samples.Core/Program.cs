8541b4ff23da4cafaec88fa5450163905d4697bf
gadhao <39508075+GadHao@users.noreply.github.com>
2021-04-05 02:39:55 +0800

    更新.gitignore文件

---
+++ Samples.Framework/Samples.Core/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Samples.Core
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            StartLongRunningTask();
+        }
+
+        public static void StartLongRunningTask()
+        {
+            // 创建一个长时间运行的Task，这将不使用线程池中的线程，而是启动一个线程池之外的额外线程
+            var longRunningTask = Task.Factory.StartNew(_ =>
+            {
+                Thread.Sleep(3000);
+                Console.WriteLine("Foo");
+            }, TaskCreationOptions.LongRunning);
+
+            longRunningTask.Wait();
+        }
+    }
+}
