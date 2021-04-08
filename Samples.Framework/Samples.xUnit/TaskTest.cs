062ab6fab76203f74f23d9cf7202e46f5cc8015c
gadhao <39508075+GadHao@users.noreply.github.com>
2021-04-08 23:58:54 +0800

    测试i++

--- Samples.Framework/Samples.xUnit/TaskTest.cs
+++ Samples.Framework/Samples.xUnit/TaskTest.cs
@@ -2,21 +2,65 @@ using System;
 using Xunit;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Samples.xUnit
 {
     public class TaskTest
     {
-        [Fact]
+        // [Fact]
         public void LongRunnigTaskTest()
         {
-             // 创建一个长时间运行的Task，这将不使用线程池中的线程，而是启动一个线程池之外的额外线程
+            // 创建一个长时间运行的Task，这将不使用线程池中的线程，而是启动一个线程池之外的额外线程
             var longRunningTask = Task.Factory.StartNew(_ =>
             {
                 Thread.Sleep(3000);
                 Console.WriteLine("Foo");
             }, TaskCreationOptions.LongRunning);
             longRunningTask.Wait();
+        }
+
+
+        [Fact]
+        public void RunTaskToAdd()
+        {
+            var tasks = new List<Task>();
+            int currentValue = 0;
+
+            // 使用Interlocked.Increment自增
+            for (int i = 0; i < 1000; i++)
+            {
+                tasks.Add(new Task(() =>
+                {
+                    for (int i = 0; i < 1000; i++)
+                    {
+                        Interlocked.Increment(ref currentValue);
+                    }
+                }));
+            }
+
+            tasks.ForEach(item => item.Start());
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine($"最后自增结果是{currentValue}");
+
+            currentValue = 0;
+            tasks.Clear();
+
+            // 使用i++自增
+            for (int i = 0; i < 1000; i++)
+            {
+                tasks.Add(new Task(() =>
+                {
+                    for (int i = 0; i < 1000; i++)
+                    {
+                        currentValue++;
+                    }
+                }));
+            }
+
+            tasks.ForEach(item => item.Start());
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine($"最后自增结果是{currentValue}");
         }
     }
 }
