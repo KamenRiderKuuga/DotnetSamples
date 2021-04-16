1bd77752a6b01b46f837e257a83a5cfa9da4a8cd
gadhao <39508075+GadHao@users.noreply.github.com>
2021-04-16 23:56:02 +0800

    update

---
+++ Samples.Framework/Samples.LittleAspNetCoreBook/Services/FakeTodoItemService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Samples.LittleAspNetCoreBook.Models;
+
+namespace Samples.LittleAspNetCoreBook.Services
+{
+    public class FakeTodoItemService : ITodoItemService
+    {
+        public Task<TodoItem[]> GetIncompleteItemsAsync()
+        {
+            var item1 = new TodoItem
+            {
+                Title = "Learn ASP.NET Core",
+                DueAt = DateTimeOffset.Now.AddDays(1)
+            };
+
+            var item2 = new TodoItem
+            {
+                Title = "Build awesome apps",
+                DueAt = DateTimeOffset.Now.AddDays(2)
+            };
+
+            return Task.FromResult(new[] { item1, item2 });
+        }
+    }
+}
---
+++ Samples.Framework/Samples.LittleAspNetCoreBook/Services/ITodoItemService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using Samples.LittleAspNetCoreBook.Models;
+
+namespace Samples.LittleAspNetCoreBook.Services
+{
+    public interface ITodoItemService
+    {
+        Task<TodoItem[]> GetIncompleteItemsAsync();
+    }
+}
