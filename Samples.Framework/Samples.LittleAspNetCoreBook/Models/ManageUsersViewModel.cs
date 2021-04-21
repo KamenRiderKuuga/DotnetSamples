f538aa88f0353a07dc823bc49b5889d098641705
gadhao <39508075+GadHao@users.noreply.github.com>
2021-04-21 23:51:28 +0800

    update

---
+++ Samples.Framework/Samples.LittleAspNetCoreBook/Models/ManageUsersViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Samples.LittleAspNetCoreBook.Models
+{
+    public class ManageUsersViewModel
+    {
+        public IdentityUser[] Administrators { get; internal set; }
+        public IdentityUser[] Everyone { get; internal set; }
+    }
+}
