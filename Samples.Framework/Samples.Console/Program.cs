4f96e0b14b8f697441792f850cd56b5c53c57339
GadHao <524472212@qq.com>
2021-07-05 11:27:27 +0800

    feat: custom text json sample

--- Samples.Framework/Samples.Console/Program.cs
+++ Samples.Framework/Samples.Console/Program.cs
@@ -3,7 +3,10 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Text.Encodings.Web;
 using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Unicode;
 
 namespace Samples.Console
 {
@@ -11,7 +14,7 @@ namespace Samples.Console
     {
         static void Main(string[] args)
         {
-            ExcuteTest(Constants.DYNAMIC_TYPE_SERIALIZE);
+            ExcuteTest(Constants.TEXT_JSON_CUSTOM);
 
             System.Console.ReadKey();
         }
@@ -30,6 +33,10 @@ namespace Samples.Console
 
                 case Constants.DYNAMIC_TYPE_SERIALIZE:
                     DynamicTypeSerialize.DynamicTypeSerializeTest();
+                    break;
+
+                case Constants.TEXT_JSON_CUSTOM:
+                    TextJsonCustom.TextJsonCustomTest();
                     break;
 
                 default:
@@ -166,12 +173,11 @@ namespace Samples.Console
             public DateTime Field3 { get; set; }
         }
 
-
         public static void DynamicTypeSerializeTest()
         {
             int tryTimes = 100000;
 
-            for (int i = 1; i <=10; i++)
+            for (int i = 1; i <= 10; i++)
             {
                 System.Console.WriteLine("第{0}轮测试，序列化动态类型对象({1}次)，{2}ms", i, tryTimes, TestUtils.TimeMethod(() =>
                 {
@@ -202,6 +208,77 @@ namespace Samples.Console
         }
     }
 
+    public class TextJsonCustom
+    {
+        public static void TextJsonCustomTest()
+        {
+            System.Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            var serverInfo = new ServerInfo()
+            {
+                ServerName = "服务器名称",
+                ServerID = 1,
+                StartTime = DateTime.Now
+            };
+
+            var content = JsonSerializer.Serialize(serverInfo, new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
+            });
+
+            System.Console.WriteLine(content);
+
+            var contentValue = JsonSerializer.Deserialize<ServerInfo>(content);
+        }
+
+        /// <summary>
+        /// 服务器信息
+        /// </summary>
+        public class ServerInfo
+        {
+            /// <summary>
+            /// 服务器名称
+            /// </summary>
+            [JsonPropertyName("server_name")]
+            public string ServerName { get; set; }
+
+            /// <summary>
+            /// 服务器ID
+            /// </summary>
+            [JsonPropertyName("server_id")]
+            public int ServerID { get; set; }
+
+            /// <summary>
+            /// start_time
+            /// </summary>
+            [JsonConverter(typeof(CustomDateTimeConverter))]
+            [JsonPropertyName("start_time")]
+            public DateTimeOffset StartTime { get; set; }
+        }
+
+        public class CustomDateTimeConverter : JsonConverter<DateTimeOffset>
+        {
+            public override DateTimeOffset Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            {
+                var content = reader.GetString();
+
+                if (long.TryParse(content, out var result))
+                {
+                    return DateTimeOffset.FromUnixTimeSeconds(result);
+                }
+                else
+                {
+                    return new DateTimeOffset();
+                }
+            }
+
+            public override void Write(Utf8JsonWriter writer, DateTimeOffset value, JsonSerializerOptions options)
+            {
+                writer.WriteStringValue(value.ToUnixTimeSeconds().ToString());
+            }
+        }
+    }
+
     public class Constants
     {
         public const string ENUMS_DESCRIPTION_TEST = "获取枚举信息测试，对比反射速度以及字典速度";
@@ -209,6 +286,8 @@ namespace Samples.Console
         public const string STATIC_FIELD_TEST = "静态变量测试，主要是为了测试其初始化时机，初始化的线程安全性，容量上限";
 
         public const string DYNAMIC_TYPE_SERIALIZE = "动态类型序列化效率测试";
+
+        public const string TEXT_JSON_CUSTOM = "System.Text.Json自定义序列化/反序列化测试";
     }
 
     public enum SampleEnums
