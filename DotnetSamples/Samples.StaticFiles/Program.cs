3944229ea4466e120bfd2b16f1feddb07e6d2498
GadHao <524472212@qq.com>
2022-01-06 23:15:36 +0800

    feat: finish the static files project

--- DotnetSamples/Samples.StaticFiles/Program.cs
+++ DotnetSamples/Samples.StaticFiles/Program.cs
@@ -1,6 +1,10 @@
 using FluentValidation.AspNetCore;
+using Microsoft.Extensions.FileProviders;
+using Samples.StaticFiles;
+using Samples.StaticFiles.AppConfigs;
 using Samples.StaticFiles.Filters;
 using Samples.StaticFiles.Models.Validators;
+using Samples.StaticFiles.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,6 +26,24 @@ builder.Services.AddControllers(option =>
     conf.RegisterValidatorsFromAssemblyContaining<FileUploadValidator>();
     conf.DisableDataAnnotationsValidation = false;
 });
+
+// 跨域配置
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder =>
+        {
+            builder.AllowAnyOrigin() // 允许所有站点跨域请求
+            .AllowAnyMethod() // 允许所有请求方法
+            .AllowAnyHeader(); // 允许所有请求头
+        });
+});
+
+builder.Services.AddScoped<FileUploadService>();
+
+builder.Services.Configure<SecretKeyConfigs>(builder.Configuration.GetSection(nameof(SecretKeyConfigs)));
+builder.Services.Configure<FileConfigs>(builder.Configuration.GetSection(nameof(FileConfigs)));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -32,6 +54,20 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
+
+var staticFileFolder = Path.Combine(AppContext.BaseDirectory, Constants.FileFolderName);
+
+if (!Directory.Exists(staticFileFolder))
+{
+    Directory.CreateDirectory(staticFileFolder);
+}
+
+app.UseStaticFiles(new StaticFileOptions()
+{
+    FileProvider = new PhysicalFileProvider(staticFileFolder),
+    RequestPath = new PathString("/files"),
+    ServeUnknownFileTypes = true
+});
 
 app.MapControllers();
 
