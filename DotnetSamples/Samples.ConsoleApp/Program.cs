0a15e618dfd14cf9ec5f495308eb8b20008af7f5
KamenRiderKuuga <524472212@qq.com>
2022-05-02 22:31:26 +0800

    feat: remove the input args

--- DotnetSamples/Samples.ConsoleApp/Program.cs
+++ DotnetSamples/Samples.ConsoleApp/Program.cs
@@ -5,7 +5,7 @@ namespace Samples.ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             ExcuteTest(Constants.MEMORYMAPPEDFILE_TEST);
             Console.ReadKey();
